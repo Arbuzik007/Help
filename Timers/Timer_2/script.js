4b1c1c47abe6fbb64e132622c5ac9e4421297894
Arbuzik007 <77507273+Arbuzik007@users.noreply.github.com>
2021-08-22 13:12:31 +0300

    Add files via upload

---
+++ Timers/Timer_2/script.js
@@ -0,0 +1,15 @@
+// Unrelated to actual timer, it's just a colour thing
+let root = document.documentElement;
+const fontColourInput = document.getElementById('fontColour');
+const bgColorInput = document.getElementById('bgColour');
+
+fontColourInput.addEventListener('input', updateFontColour, false);
+bgColorInput.addEventListener('input', updateBgColour, false);
+
+function updateFontColour(event) {
+  root.style.setProperty('--fontColour', event.target.value);
+}
+
+function updateBgColour(event) {
+  root.style.setProperty('--bgColour', event.target.value);
+}

a8e5f75e43830ebec9afebeb3f418887a9e70b02
Arbuzik007 <77507273+Arbuzik007@users.noreply.github.com>
2021-08-24 11:04:32 +0300

    Update edit.php

--- Разное/JSON/edit.php
+++ Разное/JSON/edit.php
@@ -4,11 +4,11 @@ header("Content-Type: application/json");
 // получаем данные от страницы
 $data = file_get_contents("php://input");
 // открываем наш файл с данными
-$file = file_get_contents('D:/zlata/JSON/data.json');  
+$file = file_get_contents('https://arbuzik007.github.io/Help/Разное/JSON/data.json');  
 // записываем в него то, что пришло от страницы
-file_put_contents('D:/zlata/JSON/data.json',$data);  
+file_put_contents('https://arbuzik007.github.io/Help/Разное/JSON/data.json',$data);  
 // тут же заново считываем все данные, чтобы убедиться, что всё записалось правильно
-$file = file_get_contents('D:/zlata/JSON/data.json'); 
+$file = file_get_contents('https://arbuzik007.github.io/Help/Разное/JSON/data.json.json'); 
 // и сразу отправляем их на страницу, чтобы это увидел пользователь
 echo $data;
 // освобождаем память 
